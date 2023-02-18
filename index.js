f6176f74cc75ca9c37973af47536e57540c91e53
b1ek <me@blek.codes>
2023-02-19 01:29:43 +1000

    encrypt cookies

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ require('./startup');
 
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const cookie_parse = require('cookie-parser');
+const cookie_encrypt = require('cookie-encrypter');
 
-const { APP_PORT } = process.env;
+const { APP_PORT, APP_KEY } = process.env;
 
 app.use(require('./routes'));
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.use(cookie_parse(APP_KEY))
+app.use(cookie_encrypt(APP_KEY));
+app.use(session({
+	secret: APP_KEY,
+	cookie: { secure: true }
+}));
 
 const server = app.listen(APP_PORT, () => {
 		console.log("Listening on port " + APP_PORT);
