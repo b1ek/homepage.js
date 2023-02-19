cda3d857485369995419ba83a333ee58cadd5895
b1ek <me@blek.codes>
2023-02-19 12:25:01 +1000

    use redis for sessions

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const app = express();
 const session = require('express-session');
 const cookie_parse = require('cookie-parser');
 const cookie_encrypt = require('cookie-encrypter');
+const Redis = require("ioredis");
+let redisClient = new Redis(process.env.REDIS_PORT, process.env.REDIS_HOST);
+let RedisStore = require("connect-redis")(session)
 
 const { APP_PORT, APP_KEY } = process.env;
 
@@ -17,8 +20,12 @@ app.use(cookie_parse(APP_KEY))
 app.use(cookie_encrypt(APP_KEY));
 app.use(session({
 	secret: APP_KEY,
-	cookie: { secure: true }
+	resave: false,
+	saveUninitialized: true,
+	cookie: { secure: true },
+	store: new RedisStore({ client: redisClient })
 }));
+app.use(require('./middleware'));
 
 const server = app.listen(APP_PORT, () => {
 		console.log("Listening on port " + APP_PORT);
