72942108215af335403c6d38af481e21f68ad61e
b1ek <me@blek.codes>
2023-02-22 17:55:13 +1000

    add guestbook records to index page

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,11 +1,19 @@
 const Helpers = require('../helpers');
+const Sequelize = require('../models');
 
 async function handler(req, res) {
+
+    let gb_entries = await Sequelize.Guestbook.findAll({
+        limit: 5,
+        order: [['id', 'DESC']]
+    });
+
     res.send(
         await Helpers.ViewLoader.load(
             'main.pug',
             {
-                current_route: '/'
+                current_route: '/',
+                gb_entries
             }
         )
     );
