dd0957f70087ed581d5a9bea5353b12e431bb9d8
b1ek <me@blek.codes>
2023-03-09 23:40:29 +1000

    add bolerplate resume page

--- routes/pages.js
+++ routes/pages.js
@@ -4,7 +4,11 @@ async function services(req, res) {
     res.template('page/services.pug');
 }
 
+async function resume(req, res) {
+    res.template('page/resume.pug');
+}
 
 module.exports = (router) => {
-     router.get('/services', handler(services))
+    router.get('/services', handler(services))
+    router.get('/resume', handler(resume))
 }
