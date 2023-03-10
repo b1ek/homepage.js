d46d7e28b7d4558924db7650e53921724cdc4966
b1ek <me@blek.codes>
2023-03-10 17:26:44 +1000

    add load simulation

--- react/resume/src/resume.js
+++ react/resume/src/resume.js
@@ -1,7 +1,9 @@
 import { createRoot } from "react-dom/client";
 import { Base } from './Base';
 
-document.getElementById('resume_js_app').innerHTML = '';
-const container = document.getElementById("resume_js_app");
-const root = createRoot(container)
-root.render(<Base />);
+setTimeout(() => {
+    document.getElementById('resume_js_app').innerHTML = '';
+    const container = document.getElementById("resume_js_app");
+    const root = createRoot(container)
+    root.render(<Base />);
+}, (Math.random() * 200) + 500)
