6ff1bac165e47da3187b7335f893a2a1a9a17fd5
b1ek <me@blek.codes>
2023-03-24 11:49:07 +1000

    remove leftover console log

--- routes/guestbook.js
+++ routes/guestbook.js
@@ -156,7 +156,6 @@ async function rss(req, res) {
     }]
 
     for (const record of data) {
-        console.log(record);
         if (record.email == null || record.email == undefined) {
             record.email = 'no@email.com';
         }
