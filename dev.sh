0c3ed4152e01c9a823f4a5a7e0552e75884e6007
b1ek <me@blek.codes>
2023-04-20 15:59:48 +1000

    check if build was successful in scripts

--- dev.sh
+++ dev.sh
@@ -11,6 +11,10 @@ cp docker-compose.dev docker-compose.yml
 cp Dockerfile.dev Dockerfile
 
 docker-compose build
+if [ $? != 0 ]; then
+    echo "Error while building; check logs" >&2
+    exit -1
+fi
 
 echo -e "\033[1;32mDevelopment environment set up successfully\033[0m"
 echo -e "Start it up with \033[1;32mdocker-compose up -d\033[0m!\n"
--- prod.sh
+++ prod.sh
@@ -11,6 +11,10 @@ cp docker-compose.prod docker-compose.yml
 cp Dockerfile.prod Dockerfile
 
 docker-compose build
+if [ $? != 0 ]; then
+    echo "Error while building; check logs" >&2
+    exit -1
+fi
 
 echo -e "\033[1;32mProduction environment set up successfully\033[0m"
 echo -e "Start it up with \033[1;32mdocker-compose up -d\033[0m!\n"
